9cbeafc644c1caa49f2a763a9902d686c2e17ae4
PumpkinSeed <qwer.kocka@gmail.com>
2021-03-11 15:10:09 +0100

    Add test fix

--- main_test.go
+++ main_test.go
@@ -17,7 +17,7 @@ func TestFuzz(t *testing.T) {
 		Username: "test",
 		Password: "test",
 		Database: "test",
-		Host:     "localhost",
+		Host:     "mysql",
 		Port:     "3306",
 		Driver:   "mysql",
 	}
