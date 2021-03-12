5a4e0f933d13459aa8efd1e312a55e34b665ef2b
Robert Boros <robbie13.boros@gmail.com>
2021-03-12 12:02:44 +0100

    add psql test

--- main_test.go
+++ main_test.go
@@ -11,7 +11,7 @@ import (
 	"github.com/brianvoe/gofakeit/v5"
 )
 
-func TestFuzz(t *testing.T) {
+func TestMySQL(t *testing.T) {
 	f := flags.Flags{}
 	f.Driver = drivers.Flags{
 		Username: "test",
@@ -20,6 +20,43 @@ func TestFuzz(t *testing.T) {
 		Host:     "localhost",
 		Port:     "3306",
 		Driver:   "mysql",
+	}
+	f.Table = "Persons"
+	f.Parsed = true
+
+
+
+	gofakeit.Seed(0)
+	driver := drivers.New(f.Driver)
+	testable := drivers.NewTestable(f.Driver)
+	db := connector.Connection(driver)
+
+	if _, err := db.Exec(fmt.Sprintf("DROP TABLE IF EXISTS %s", f.Table)); err != nil {
+		t.Fatal(err)
+	}
+	if err := testable.TestTable(db, f.Table); err != nil {
+		t.Fatal(err)
+	}
+	fields, err := driver.DescribeFields(f.Table, db)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	defer db.Close()
+	err = fuzzer.Run(db, fields, f)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestPSQL(t *testing.T) {
+	f := flags.Flags{}
+	f.Driver = drivers.Flags{
+		Username: "test",
+		Password: "test",
+		Database: "test",
+		Host:     "localhost",
+		Port:     "5432",
+		Driver:   "postgres",
 	}
 	f.Table = "Persons"
 	f.Parsed = true
