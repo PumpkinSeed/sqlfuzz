cb918fd44349c778149a1e32a5d1a497907c0441
Srinidhi <srinidhi.s@amagi.com>
2021-03-16 23:20:40 +0530

    Fix for broken CI/CD Pipeline

--- drivers/postgres_example_test.go
+++ drivers/postgres_example_test.go
@@ -6,62 +6,6 @@ import (
 	"log"
 )
 
-const CreateTable = `CREATE TABLE IF NOT EXISTS pg_data_types (
-   col1 bigint,
-   col2 int8,
-   col3 bit(10),
-   col4 bit,
-   col5 bit varying,
-   col6 bit varying(10),
-   col61 bit varying(128),
-   col62 bit varying(256),
-   col63 bit varying(512),
-   col64 bit varying(1024),
-   col7 varbit,
-   col8 varbit(10),
-   col9 boolean,
-   col10 bool,
-   col11 bytea,
-   col12 character,
-   col13 character(10),
-   col14 char,
-   col15 char(10),
-   col16 character varying,
-   col17 character varying(10),
-   col18 varchar,
-   col19 varchar(10),
-   col20 cidr,
-   col21 date,
-   col22 double precision,
-   col23 float8,
-   col24 inet,
-   col25 integer,
-   col26 int,
-   col27 int4,
-   col28 json,
-   col29 jsonb,
-   col30 line,
-   col31 macaddr,
-   col32 money,
-   col33 numeric(5,2),
-   col34 decimal(5,2),
-   col35 real,
-   col36 float4,
-   col37 smallint,
-   col38 int2,
-   col39 smallserial,
-   col40 serial2,
-   col41 serial,
-   col42 serial4,
-   col43 text,
-   col44 time,
-   col45 timetz,
-   col46 timestamp,
-   col47 uuid,
-   col48 xml
-);
-`
-
 func getConnection() (*sql.DB, error) {
 	connectionString := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable ", "127.0.0.1", "5432", "postgres", "password", "fuzzpostgres")
 	db, err := sql.Open("postgres", connectionString)
@@ -76,7 +20,7 @@ func createTable() {
 	if err != nil {
 		return
 	}
-	db.Query(CreateTable)
+	db.Query(fmt.Sprintf(CreateTable, "test_table"))
 }
 
 func ExamplePostgres_ShowTables() {
--- main_test.go
+++ main_test.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	"fmt"
-	"testing"
-
 	"github.com/PumpkinSeed/sqlfuzz/drivers"
 	"github.com/PumpkinSeed/sqlfuzz/pkg/connector"
 	"github.com/PumpkinSeed/sqlfuzz/pkg/flags"
 	"github.com/PumpkinSeed/sqlfuzz/pkg/fuzzer"
 	"github.com/brianvoe/gofakeit/v5"
+	"testing"
 )
 
 func TestFuzz(t *testing.T) {
@@ -70,19 +69,27 @@ func TestFuzz(t *testing.T) {
 func TestFuzzPostgres(t *testing.T) {
 	f := flags.Flags{}
 	f.Driver = drivers.Flags{
-		Username: "postgres",
-		Password: "password",
-		Database: "fuzzpostgres",
+		Username: "test",
+		Password: "test",
+		Database: "test",
 		Host:     "127.0.0.1",
 		Port:     "5432",
 		Driver:   "postgres",
 	}
-	f.Table = "pg_data_types"
+	f.Table = "Persons"
 	f.Parsed = true
 
 	gofakeit.Seed(0)
 	driver := drivers.New(f.Driver)
+	testable := drivers.NewTestable(f.Driver)
 	db := connector.Connection(driver)
+
+	if _, err := db.Exec(fmt.Sprintf("DROP TABLE IF EXISTS %s", f.Table)); err != nil {
+		t.Fatal(err)
+	}
+	if err := testable.TestTable(db, f.Table); err != nil {
+		t.Fatal(err)
+	}
 	fields, err := driver.DescribeFields(f.Table, db)
 	if err != nil {
 		t.Fatal(err.Error())
@@ -91,6 +98,37 @@ func TestFuzzPostgres(t *testing.T) {
 	err = fuzzer.Run(db, fields, f)
 	if err != nil {
 		t.Fatal(err)
+	}
+	res, err := db.Query(fmt.Sprintf("SELECT * FROM %s", f.Table))
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer res.Close()
+	var i int
+	for res.Next() {
+		columns, err := res.Columns()
+		if err != nil {
+			t.Error(err)
+		}
+		var valueHolders []interface{}
+		for j := 0; j < len(columns); j++ {
+			var tempInterface interface{}
+			valueHolders = append(valueHolders, &tempInterface)
+		}
+		if err := res.Scan(valueHolders...); err != nil {
+			t.Error(err)
+			continue
+		}
+		for index, val := range valueHolders {
+			// If given column index is valid and value is nil, return error
+			if !drivers.PostgresInvalidIndicesMap[index] && val == nil {
+				t.Error(fmt.Sprintf("Invalid value received for column index %d", index))
+			}
+		}
+		i++
+	}
+	if i == 0 {
+		t.Error("the table should not be empty")
 	}
 }
 
